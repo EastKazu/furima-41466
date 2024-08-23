19e6c86dd0433685ef088717028786916171027e
EastKazu <fhornest55@gmail.com>
2024-08-23 19:13:20 +0900

    パスワードを半角英数混合にするバリデーションを記載

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
 
   validates :nickname, presence: true
+  validates :password, presence: true, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}\z/, message: "は半角英数字混合で6文字以上にしてください" }
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :first_name
     validates :last_name
