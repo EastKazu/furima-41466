e1f0a78b01a324be94726fe0f278acb38a83e032
EastKazu <fhornest55@gmail.com>
2024-08-26 18:28:16 +0900

    passwordのテストコードを修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe User, type: :model do
         expect(@user).to be_valid
       end
     end
-    
+
     context '新規登録できないとき' do
       it 'nicknameが空では登録できない' do
         @user.nickname = ''
@@ -59,10 +59,22 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include('Password is too long (maximum is 128 characters)')
       end
 
-      it 'passwordが半角英数混合でないと登録できない' do
+      it 'passwordが英字のみだと登録できない' do
+        @user.password = 'abcdef'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password は半角英数字混合で6文字以上にしてください")
+      end
+
+      it 'passwordが数字のみだと登録できない' do
         @user.password = '123456'
         @user.valid?
-        expect(@user.errors.full_messages).to include('Password は半角英数字混合で6文字以上にしてください')
+        expect(@user.errors.full_messages).to include("Password は半角英数字混合で6文字以上にしてください")
+      end
+
+      it 'passwordに全角文字が含まれると登録できない' do
+        @user.password = '1234Ａa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password は半角英数字混合で6文字以上にしてください")
       end
 
       it 'passwordとpassword_confirmationが不一致では登録できない' do
