f6ce3dd6e5a0f042719c323a7b8b7fe038b1f994
EastKazu <fhornest55@gmail.com>
2024-08-26 17:57:53 +0900

    passwordに記載されたpresence: trueの記述を消去

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :password, presence: true,
-                       format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}\z/, message: 'は半角英数字混合で6文字以上にしてください' }
+  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}\z/, message: 'は半角英数字混合で6文字以上にしてください' }
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
     validates :first_name
     validates :last_name
