8b47bebfc141e562f206bed486badc00635517ea
EastKazu <fhornest55@gmail.com>
2024-08-27 19:02:44 +0900

    販売利益表示の実装

--- app/javascript/calculation.js
+++ app/javascript/calculation.js
@@ -2,12 +2,13 @@ window.addEventListener('load', function(){
 
   const priceInput = document.getElementById("item-price")
   const addTaxPrice = document.getElementById('add-tax-price')
-  
+  const addProfit = document.getElementById('profit')
+
   priceInput.addEventListener('input', function(){
     const price = parseFloat(priceInput.value) 
-    const tax = Math.floor(price * 0.10)            
+    const tax = Math.floor(price * 0.10)
+    const profit = price - tax            
     addTaxPrice.textContent = tax;
+    addProfit.textContent = profit;
   })
-
-
 })
