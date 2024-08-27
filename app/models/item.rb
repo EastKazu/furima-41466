2a845d2e5bc2b1f394d4395ebe34c95346fefce3
EastKazu <fhornest55@gmail.com>
2024-08-27 18:17:39 +0900

    出品価格の制限と半角数値のみ入力可能に変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,9 @@ class Item < ApplicationRecord
   validates :postage_id, numericality: { other_than: 1, message: "can't be blank" } 
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" } 
   validates :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" } 
-  validates :price, presence: true
+  validates :price, presence: true, 
+    numericality:{ greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "must be between 300 and 9999999" },
+    format: { with: /\A[0-9]+\z/, message: "must be a number" }
   validates :user, presence: true
 
   belongs_to :user
