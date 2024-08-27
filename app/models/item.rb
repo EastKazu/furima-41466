6faf0d1afb15fb69cfaeb2708c62cd4966fed232
EastKazu <fhornest55@gmail.com>
2024-08-27 21:34:51 +0900

    imageのバリデーションを記入

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,6 +10,7 @@ class Item < ApplicationRecord
     numericality:{ greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "must be between 300 and 9999999" },
     format: { with: /\A[0-9]+\z/, message: "must be a number" }
   validates :user, presence: true
+  validates :image, presence: true
 
   belongs_to :user
   has_one_attached :image
