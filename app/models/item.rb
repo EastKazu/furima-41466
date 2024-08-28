c723657e76a61fcb8dce11f64c4bd5163aefedc0
EastKazu <fhornest55@gmail.com>
2024-08-28 16:15:18 +0900

    userに対するバリデーションを消去

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,6 @@ class Item < ApplicationRecord
   validates :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" } 
   validates :price, presence: true,
     numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "must be a half-width number between 300 and 9999999" }
-  validates :user, presence: true
   validates :image, presence: true
 
   belongs_to :user
