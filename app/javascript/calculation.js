6eb66aea6863c3797aea737b6e2d4c68fcfdee09
EastKazu <fhornest55@gmail.com>
2024-08-28 16:10:12 +0900

    販売手数料と販売利益の計算をrenderメソッドに対応

--- app/javascript/calculation.js
+++ app/javascript/calculation.js
@@ -1,14 +1,18 @@
-window.addEventListener('load', function(){
+const price = () => {
+  const priceInput = document.getElementById("item-price");
+  if (priceInput) {
+    const addTaxPrice = document.getElementById('add-tax-price');
+    const addProfit = document.getElementById('profit');
 
-  const priceInput = document.getElementById("item-price")
-  const addTaxPrice = document.getElementById('add-tax-price')
-  const addProfit = document.getElementById('profit')
+    priceInput.addEventListener('input', function() {
+      const price = parseFloat(priceInput.value);
+      const tax = Math.floor(price * 0.10);
+      const profit = price - tax;
+      addTaxPrice.textContent = tax;
+      addProfit.textContent = profit;
+    });
+  }
+};
 
-  priceInput.addEventListener('input', function(){
-    const price = parseFloat(priceInput.value) 
-    const tax = Math.floor(price * 0.10)
-    const profit = price - tax            
-    addTaxPrice.textContent = tax;
-    addProfit.textContent = profit;
-  })
-})
+window.addEventListener("turbo:load", price);
+window.addEventListener("turbo:render", price);
